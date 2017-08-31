3d391199083bf983a3202d5e92d03967d7f7d170
rdom <r.dzhygadlo@gsi.de>
2017-08-31 16:16:11 +0200

    sync

--- js/prtonlines.js
+++ js/prtonlines.js
@@ -2,13 +2,13 @@
 // contact: r.dzhygadlo at gsi.de
 
 var gOnline = 1;
+var gpicid="data/last_hhp?";
 
 
 $(document).ready(function(){
 
     var canv = document.getElementById('canv').getContext('2d');
     var iprt = new Image();
-    
     
     var svg = d3.select("svg"),
     margin = {top: 20, right: 20, bottom: 110, left: 80},
@@ -54,6 +54,9 @@ $(document).ready(function(){
     var focus = svg.append("g")
         .attr("class", "focus")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var dots = focus.append("g");
+    dots.attr("clip-path", "url(#clip)");    
     
     var context = svg.append("g")
         .attr("class", "context")
@@ -107,19 +110,22 @@ $(document).ready(function(){
 
 	adata=data;
 
-	 x.domain(d3.extent(data, function(d) { return d.time; }));
-	 y.domain([0, d3.max(data, function(d) { return d.total; })]);
 	
-	// x.domain([data[0].time, data[data.length - 1].time]);
-	// y.domain(d3.extent(data, function(d) { return d.total; }));
-	
+	x.domain(d3.extent(adata, function(d) { return d.time; }));
+	y.domain([0, d3.max(adata, function(d) { return d.total; })]);
+		
 	x2.domain(x.domain());
 	y2.domain(y.domain());
 
 	focus.append("path")
-	    .datum(data)
+	    .datum(adata)
 	    .attr("class", "area")
 	    .attr("d", area);
+	
+	context.append("path")
+	    .datum(adata)
+	    .attr("class", "area2")
+	    .attr("d", area2);
 	
 	focus.append("g")
 	    .attr("class", "axis axis--x")
@@ -130,11 +136,6 @@ $(document).ready(function(){
 	    .attr("class", "axis axis--y")
 	    .call(yAxis);
 
-	context.append("path")
-	    .datum(data)
-	    .attr("class", "area")
-	    .attr("d", area2);
-
 	context.append("g")
 	    .attr("class", "axis axis--x")
 	    .attr("transform", "translate(0," + height2 + ")")
@@ -144,6 +145,8 @@ $(document).ready(function(){
 	    .attr("class", "brush")
 	    .call(brush)
 	    .call(brush.move, x.range());
+
+//	context.select(".brush").call(brush.move, [ adata[adata.length - 100].time, adata[adata.length - 1].time].map(x));
 
 	svg.append("rect")
 	    .attr("class", "zoom")
@@ -164,38 +167,36 @@ $(document).ready(function(){
 	    .call(zoom);
 
 	// append scatter plot to main chart area
-	var dots = focus.append("g");
-	dots.attr("clip-path", "url(#clip)");
-	dots.selectAll("dot")
-            .data(data)
-            .enter().append("circle")
-            .attr('class', 'dot')
-            .attr("r",3)
-            .attr("cx", function(d) { return x(d.time); })
-            .attr("cy", function(d) { return y(d.total); })
-
+	dots.selectAll("path")
+	    .data(adata)
+	    .enter().append("path")
+	    .attr('class', 'dott')
+	    .attr("transform", function(d) { return "translate(" + x(d.time) + "," + y(d.total) + ")"; })
+	    .attr("d", d3.symbol().size(40));	
 
 	function mousemove() {
 	var x0 = x.invert(d3.mouse(this)[0]),
-	    i = bisectDate(data, x0, 1),
-	    d0 = data[i - 1],
-	    d1 = data[i],
+	    i = bisectDate(adata, x0, 1),
+	    d0 = adata[i - 1],
+	    d1 = adata[i],
 	    d = x0 - d0.time > d1.time - x0 ? d1 : d0;
 	    mfocus.attr("transform", "translate(" + (margin.left +x(d.time)) + "," + (margin.top + y(d.total)) + ")");
 	    vline.attr("transform", "translate(" + (margin.left+ x(d.time)) + "," + margin.top + ")");
 	mfocus.select("text").text(d.time.getTime()/1000);
 	    if(gOnline==0){
-		console.log("data/pics/digi_"+d.time.getTime()/1000+".png");
-		iprt.src = "data/pics/digi_"+d.time.getTime()/1000+".png";
+		iprt.src = "data/pics/hhp_"+d.time.getTime()/1000+".png";
 		iprt.onload = function(){ canv.drawImage(iprt, 0, 0); }
 	    }
     }
 
     });
 
-
+    
     function brushed() {
 	if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+
+	//	brush.extent([0, width/2]);
+	
 	var s = d3.event.selection || x2.range();
 	x.domain(s.map(x2.invert, x2));
 	focus.select(".area").attr("d", area);
@@ -203,11 +204,11 @@ $(document).ready(function(){
 	svg.select(".zoom").call(zoom.transform, d3.zoomIdentity
 				 .scale(width / (s[1] - s[0]))
 				 .translate(-s[0], 0));
-
-	focus.selectAll(".dot")
-	    .attr("cx", function(d) { return x(d.time); })
-	    .attr("cy", function(d) { return y(d.total); });
 	
+	dots.selectAll(".dott")
+	    .attr("transform", function(d) { return "translate(" + x(d.time) + "," + y(d.total) + ")"; })
+	    .attr("d", d3.symbol().size(40));
+
     }
 
     function zoomed() {
@@ -217,10 +218,10 @@ $(document).ready(function(){
 	focus.select(".area").attr("d", area);
 	focus.select(".axis--x").call(xAxis);
 
-
-	focus.selectAll(".dot")
-	    .attr("cx", function(d) { return x(d.time); })
-	    .attr("cy", function(d) { return y(d.total); });
+	dots.selectAll(".dott")
+	    .attr("transform", function(d) { return "translate(" + x(d.time) + "," + y(d.total) + ")"; })
+	    .attr("d", d3.symbol().size(40));
+	
 
 	context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
     }
@@ -234,37 +235,66 @@ $(document).ready(function(){
     setInterval(function(){ readData();}, 1000);
     var ttt =1;
     function readData(){
-	d3.csv("data/last_timeline", function(newdata) {
-	    newdata.forEach(function(d) {
-		d.time = new Date(d.time*1000);
-		d.total = +d.total/1000;
-	    });
 
-	    if(adata[adata.length - 1].time != newdata[0].time && adata[adata.length - 1].total != newdata[0].total){
-		adata.push(newdata[0]);
-		svg.select("g.x.axis").call(xAxis);
-		svg.select("path.line").attr("d", line(adata));
-	    }
-	});
+	d3.csv("data/last_timeline", function(newdata) {
+    	    newdata.forEach(function(d) {
+    		d.time = new Date(d.time*1000);
+    		d.total = +d.total/1000;
+    	    });
+
+    	    if(adata[adata.length - 1].time != newdata[0].time && adata[adata.length - 1].total != newdata[0].total){
+
+    		adata.push(newdata[0]);
+
+    		focus.select("path.area").attr("d", area(adata));
+    		context.select("path.area2").attr("d", area2(adata));
+
+    		dots.selectAll("path")
+    		    .data(adata)
+    		    .enter().append("path")
+    		    .attr('class', 'dott')
+    		    .attr("transform", function(d) { return "translate(" + x(d.time) + "," + y(d.total) + ")"; })
+    		    .attr("d", d3.symbol().size(40));
+		
+
+    		dots.selectAll(".dott")
+    		    .attr("transform", function(d) { return "translate(" + x(d.time) + "," + y(d.total) + ")"; })
+    		    .attr("d", d3.symbol().size(40));
+    	    }
+    	});
+	
 
 	if(gOnline==1){
-	    x.domain([x.domain()[0], adata[adata.length - 1].time]);
-	    y.domain([0, d3.max(adata, function(d) { return d.total; })]);
 
-	    iprt.src = "data/last_digi?" + escape(new Date());
+	    x.domain(d3.extent(adata, function(d) { return d.time; }));
+	    y.domain([0, d3.max(adata, function(d) { return d.total; })]);
+	    
+	    x2.domain(x.domain());
+	    y2.domain(y.domain());
+
+	    if(adata.length>100){
+		context.select(".brush").call(brush.move, [ adata[adata.length - 100].time, adata[adata.length - 1].time].map(x));
+	    }
+
+	    iprt.src = gpicid + escape(new Date());
 	    iprt.onload = function(){ canv.drawImage(iprt, 0, 0); }
 	}
     }
-        
     
 });
 
+
 function prtOnline(){
-    if(gOnline == 1){
+    if(gOnline == 1){	
 	gOnline = 0;
 	d3.select("#btnOnline").text("Offline");
     }else{
 	gOnline = 1;
 	d3.select("#btnOnline").text("Online");
     }
+}
+
+
+function prtPics(picid){
+    gpicid = "data/last_"+picid +"?";
 }
